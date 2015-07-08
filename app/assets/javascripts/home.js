05a6a8c0f269037df8f2c9db8487a4b12773e042
Ariel Pérez <arielperezch@gmail.com>
2015-07-08 15:38:33 -0300

    Fixed sprites not updating to disabled when the autocomplete is used

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -25,42 +25,46 @@ function initializeDnD() {
         }
     });
 }
-function initializeQuery() {
-    $('#query').on('input', function() {
-        var query = $('#query').val();
-        var lowerQuery = query.toLowerCase();
 
-        var hintSet = false;
-        $('.sprite').each(function() {
-            var name = $(this).attr('data-original-title');
-            var lowerName = name.toLowerCase();
+function updateSprites() {
+    var query = $('#query').val();
+    var lowerQuery = query.toLowerCase();
 
-            if(lowerName.includes(lowerQuery)) {
-                $(this).removeClass('disabled');
-                $(this).draggable('enable');
+    var hintSet = false;
+    $('.sprite').each(function() {
+        var name = $(this).attr('data-original-title');
+        var lowerName = name.toLowerCase();
 
-                if(!hintSet && query.length > 0 && lowerName.startsWith(lowerQuery)) {
-                    $('#query-hint').val(query + name.substring(query.length));
-                    $('#query-hint').attr('original-value', name);
-                    hintSet = true;
-                }
+        if(lowerName.includes(lowerQuery)) {
+            $(this).removeClass('disabled');
+            $(this).draggable('enable');
+
+            if(!hintSet && query.length > 0 && lowerName.startsWith(lowerQuery)) {
+                $('#query-hint').val(query + name.substring(query.length));
+                $('#query-hint').attr('original-value', name);
+                hintSet = true;
             }
-            else {
-                $(this).addClass('disabled');
-                $(this).draggable('disable');
-            }
-        });
-
-        if (!hintSet) {
-            $('#query-hint').val(query);
-            $('#query-hint').attr('original-value', query);
+        }
+        else {
+            $(this).addClass('disabled');
+            $(this).draggable('disable');
         }
     });
+
+    if (!hintSet) {
+        $('#query-hint').val(query);
+        $('#query-hint').attr('original-value', query);
+    }
+}
+
+function initializeQuery() {
+    $('#query').on('input', updateSprites);
 
     $('#query').on('keydown', function(e) {
         if (e.which == 9) {
             $('#query').val($('#query-hint').attr('original-value'));
             $('#query-hint').val($('#query-hint').attr('original-value'));
+            updateSprites();
             e.preventDefault();
         }
     });
